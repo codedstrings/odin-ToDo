321881c60a95b59b40b1d87bc9ffd6d541756388
codedstrings <mridhulml@gmail.com>
2024-09-02 07:28:41 +0530

    add: basic layout for the page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
-import mainMenu from "./Modules/consoleApp.js"
+// import mainMenu from "./Modules/consoleApp.js"
+import '../assets/styles.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-mainMenu();
+
+
+// mainMenu(); //use to call the consoleApp(uncomment import statement).
